ebf2e708f81d92c27be40eb5f02dac8b82d4f4e8
Brian McKeever <keever@gmail.com>
2023-10-10 09:12:14 -0600

    improved tinkering script

--- raise_tinkering.py
+++ raise_tinkering.py
@@ -8,17 +8,16 @@ trash_items = [
     ]
 
 Items.UseItem(source_container)
-
+Misc.Pause(1000)
 while Player.GetRealSkillValue("Tinkering") < Player.GetSkillCap("Tinkering"):
-    Misc.Pause(1000)
     ingots = Items.FindByID(0x1BF2, 0, Player.Backpack.Serial)
     if not ingots or ingots.Amount < 10:
         ingots = Items.FindByID(0x1BF2, 0, source_container)
         Items.Move(ingots, Player.Backpack, 100)
         Misc.Pause(1000)
     boards = Items.FindByID(0x1BD7, 0, Player.Backpack.Serial)
-    if not boards or boards.Amount < 10:
-        ingots = Items.FindByID(0x1BF2, 0, source_container)
+    if Player.GetSkillValue("Tinkering") < 0 + 50 and (not boards or boards.Amount < 10):
+        boards = Items.FindByID(0x1BD7, 0, source_container)
         Items.Move(boards, Player.Backpack, 100)
         Misc.Pause(1000)
         
@@ -30,35 +29,30 @@ while Player.GetRealSkillValue("Tinkering") < Player.GetSkillCap("Tinkering"):
             
     tool = get_tinker_tool()
     Items.UseItem(tool)
-    Misc.Pause(1000)
+    #Misc.Pause(1000)
     if Player.GetSkillValue("Tinkering") < 0 + 50: #NOT SURE ABOUT 50
-        Gumps.WaitForGump(949095101, 10000)
+        Gumps.WaitForGump(949095101, 2000)
         Gumps.SendAction(949095101, 50)
-        Gumps.WaitForGump(949095101, 10000)
+        Gumps.WaitForGump(949095101, 2000)
         Gumps.SendAction(949095101, 30) #axle
-        Gumps.WaitForGump(949095101, 10000)
-        Gumps.SendAction(949095101, 0)
+        Gumps.WaitForGump(949095101, 2000)
+        #Gumps.SendAction(949095101, 0)
         continue
-    if Player.GetSkillValue("Tinkering") < 10 + 50:
-        Gumps.WaitForGump(949095101, 10000) 
+    if Player.GetSkillValue("Tinkering") < 10 + 50 and len(Player.Backpack.Contains) <= 100:
+        Gumps.WaitForGump(949095101, 2000) 
         Gumps.SendAction(949095101, 29)
-        Gumps.WaitForGump(949095101, 10000)
+        Gumps.WaitForGump(949095101, 2000)
         Gumps.SendAction(949095101, 177) #tinker tools
-        Gumps.WaitForGump(949095101, 10000)
-        Gumps.SendAction(949095101, 0)
-        print(len(Player.Backpack.Contains))
-        if len(Player.Backpack.Contains) > 100:
-            tool = get_tinker_tool()
-            Items.Move(tool, trash_barrel, 1)
-            Misc.Pause(1000)
+        Gumps.WaitForGump(949095101, 2000)
+        #Gumps.SendAction(949095101, 0)
         continue
     if Player.GetSkillValue("Tinkering") < 45 + 50:
-        Gumps.WaitForGump(949095101, 10000)
+        Gumps.WaitForGump(949095101, 2000)
         Gumps.SendAction(949095101, 29)
-        Gumps.WaitForGump(949095101, 10000) 
+        Gumps.WaitForGump(949095101, 2000) 
         Gumps.SendAction(949095101, 72) #lockpick
-        Gumps.WaitForGump(949095101, 10000)
-        Gumps.SendAction(949095101, 0)
+        Gumps.WaitForGump(949095101, 2000)
+        #Gumps.SendAction(949095101, 0)
         if Player.Weight > Player.MaxWeight or Player.Weight > 400:
             lockpicks = Items.FindByID(0x14FC, 0, Player.Backpack.Serial)
             if lockpicks:
@@ -66,12 +60,12 @@ while Player.GetRealSkillValue("Tinkering") < Player.GetSkillCap("Tinkering"):
                 Misc.Pause(1000)
         continue
     if Player.GetSkillValue("Tinkering") < 50 + 50:
-        Gumps.WaitForGump(949095101, 10000)
+        Gumps.WaitForGump(949095101, 2000)
         Gumps.SendAction(949095101, 29)
-        Gumps.WaitForGump(949095101, 10000)
+        Gumps.WaitForGump(949095101, 2000)
         Gumps.SendAction(949095101, 23) #flour sifter
-        Gumps.WaitForGump(949095101, 10000)
-        Gumps.SendAction(949095101, 0)
+        Gumps.WaitForGump(949095101, 2000)
+        #Gumps.SendAction(949095101, 0)
         continue
     Misc.Beep()
     halt
