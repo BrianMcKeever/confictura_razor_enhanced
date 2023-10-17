de33d574d91cee74fb8b5a949723930654fb755b
Brian McKeever <keever@gmail.com>
2023-10-16 21:32:36 -0600

    made raise necro work after 100

--- raise_necromancy.py
+++ raise_necromancy.py
@@ -1,26 +1,21 @@
-from create_food import fuel
 
 while Player.GetSkillValue("Necromancy") < 20:
     Spells.CastNecro("Curse Weapon")
     Misc.Pause(1000)
 while Player.GetSkillValue("Necromancy") < 60:
-    fuel()
     if Player.Hits > 100:
         Spells.CastNecro("Pain Spike")
         Target.WaitForTarget(5000)
         Target.Self()
     Misc.Pause(2000)
 while Player.GetSkillValue("Necromancy") < 70:
-    fuel()
     Spells.CastNecro("Horrific Beast")
     Misc.Pause(1000)
 Spells.CastNecro("Wraith Form")
 while Player.GetSkillValue("Necromancy") < 90:
-    fuel()
     Spells.CastNecro("Wither")
     Misc.Pause(1000)
-while Player.GetSkillValue("Necromancy") < 100:
-    fuel()
+while Player.GetSkillValue("Necromancy") < Player.GetSkillCap("Necromancy"):
     if Player.Hits < 100:
         Player.UseSkill("Spirit Speak")
         Misc.Pause(3000)
