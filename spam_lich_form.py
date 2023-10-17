d784789ed287fad5f714727d44c382d4d6efd69f
Brian McKeever <keever@gmail.com>
2023-10-16 21:37:13 -0600

    now tanks karma faster and deeper

--- spam_lich_form.py
+++ spam_lich_form.py
@@ -1,3 +1,11 @@
+Journal.Clear()
 while True:
-    Spells.CastNecro("Lich Form")
+    Spells.CastNecro("Curse Weapon")
     Misc.Pause(1000)
+    if not Journal.Search("You have"):
+        break
+    Journal.Clear()
+while True:
+    Spells.CastNecro("Vampiric Form")
+    Misc.Pause(1000)
+    
